cf2b0bf08ba63dc06bfd9d7c7e39e0348df08a52
Mo-Khalaf <mohamed.khalaf1212@outlook.com>
2022-06-26 17:31:38 +0200

    connect print order in follow order screen

--- views_mangers/followOrder_manger.py
+++ views_mangers/followOrder_manger.py
@@ -1,8 +1,13 @@
-from PyQt5 import QtWidgets , QtCore
+from PyQt5 import QtWidgets, QtCore, QtPrintSupport, QtGui
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QTextTableFormat, QTextLength, QFont
+
 from views import followOrder_view
 import json
 import requests
 import os
+import pandas as pd
+from jinja2 import Template
 
 class FollowOrder(QtWidgets.QWidget, followOrder_view.Ui_Form):
     checkAcceptedSignal = QtCore.pyqtSignal()
@@ -17,6 +22,7 @@ class FollowOrder(QtWidgets.QWidget, followOrder_view.Ui_Form):
         self.searchByDate_url = "https://saied.pythonanywhere.com/searchByDate/"
         self.token = ''
         self.orderID = 0
+        self.orders = []
         self.item_selected = False
         self.listWidget.itemSelectionChanged.connect(self.selectionChanged)
 
@@ -25,6 +31,7 @@ class FollowOrder(QtWidgets.QWidget, followOrder_view.Ui_Form):
         self.delete_btn.clicked.connect(self.delete_order)
         self.next_btn.clicked.connect(self.search)
         self.reload_signal.connect(self.run)
+        self.print_btn.clicked.connect(self.print_order)
 
         self.headers = {}
 
@@ -47,12 +54,59 @@ class FollowOrder(QtWidgets.QWidget, followOrder_view.Ui_Form):
         except Exception as d:
             print("dd",d)
 
+    def print_order(self):
+        msg = QtWidgets.QMessageBox()
+        if self.item_selected != False:
+            try:
+                for item in self.orders:
+                    if item['order_id'] == self.orderID:
+                        pd_object = pd.DataFrame.from_dict(item, orient='index')
+                        df = pd.DataFrame(pd_object)
+                        df.drop(['user_id', 'client_id'], inplace=True)
+
+                        # print(df)
+                        dialog = QtPrintSupport.QPrintDialog()
+                        if dialog.exec_() == QtWidgets.QDialog.Accepted:
+                            tableFormat = QTextTableFormat()
+                            tableFormat.setAlignment(QtCore.Qt.AlignVCenter) # Justify Table
+                            tableFormat.setBackground(QtGui.QColor('#e0e0e0'))
+                            tableFormat.setCellPadding(10)
+                            tableFormat.setCellSpacing(0)
+                            tableFormat.setBorder(1)
+                            tableFormat.setWidth(QTextLength(QTextLength.PercentageLength, 100))
+                            document = QtGui.QTextDocument()
+                            cursor = QtGui.QTextCursor(document)
+                            table = cursor.insertTable( df.count(), 2, tableFormat)
+                            # format = table.format()
+                            format = table.format()
+                            format.setHeaderRowCount(0)
+                            table.setFormat(format)
+                            format = cursor.blockCharFormat()
+                            format.setFontWeight(QFont.Bold)
+                            for row, col in df.iterrows():
+                                for value in [row, col[0]]:
+                                    cursor.setCharFormat(format)
+                                    cursor.insertText(str(value))
+                                    cursor.movePosition(QtGui.QTextCursor.NextCell)
+
+                            cell = table.cellAt(1, 0)
+
+                        document.print_(dialog.printer())
+
+            except Exception as e:
+                print(e)
+        else:
+            msg.setWindowTitle("Warning")
+            msg.setText("you must select the order !")
+            msg.exec_()
+
 
     def run(self):
         self.headers = {'Accept': 'application/json; indent=4', 'Content-Type': 'application/json',
                    'Authorization': f'Token {self.token}'}
         try :
             self.reply = requests.get(self.base_url , headers=self.headers).json()
+            self.orders = self.reply
         except Exception as s :
             print("ss",s)
         try :
